5b7efb6ebbf7c1c8d11f1cef4814e44de3adc62b
ogjex <m.agnu.ha@gmail.com>
2026-04-07 18:27:24 +0200

    updated fzf script (untested)

--- fzf-command-bookmarks.sh
+++ fzf-command-bookmarks.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 # main repo: https://github.com/dzervas/fzf-command-bookmarks
 
-if [ -n "$BASH_VERSION" ]; then
-    CURRENT_SHELL="bash"
-elif [ -n "$ZSH_VERSION" ]; then
-    CURRENT_SHELL="zsh"
-else
-    CURRENT_SHELL="unknown"
-fi
-
-# following two hashes do not work on nixos
+# the following two hashes do not work on nixos
 # hash fzf 2> /dev/null || echo "[fzf-command-bookmarks] fzf binary not found!"
 # hash highlight 2> /dev/null || echo "[fzf-command-bookmarks] highlight binary not found!"
 
@@ -17,12 +9,38 @@ export FZF_COMMAND_BOOKMARKS_FILE="${HOME}/scripts/.fzf-command-bookmarks.txt"
 export FZF_COMMAND_BOOKMARKS_ADD="\C-b"
 export FZF_COMMAND_BOOKMARKS_SHOW="\C-@"
 
+check_dependencies() {
+    local dependencies=("fzf" "highlight")  # Accept all arguments as an array
+    local missing=()
 
+    for dep in "${dependencies[@]}"; do
+        if ! type -a "$dep" &> /dev/null; then
+            missing+=("$dep")
+        fi
+    done
+
+    if [ ${#missing[@]} -gt 0 ]; then
+        echo "Error: The following dependencies are missing:"
+        for m in "${missing[@]}"; do
+            echo "  - $m"
+        done
+        exit 1
+}
+
+function get_shell() {
+    if [ -n "$BASH_VERSION" ]; then
+	current_shell="bash"
+    elif [ -n "$ZSH_VERSION" ]; then
+	current_shell="zsh"
+    else
+	current_shell="unknown"
+    fi
+}
 # Readline magic helper for bash
 # Found here: https://github.com/junegunn/fzf/wiki/examples#with-write-to-terminal-capabilities
 function __ehc() {
 	if [[ -n $1 ]]; then
-		bind '"\er": redraw-current-line'
+		bind '"\er": redraw-rurrent-line'
 		bind '"\e^": magic-space'
 		READLINE_LINE=${READLINE_LINE:+${READLINE_LINE:0:READLINE_POINT}}${1}${READLINE_LINE:+${READLINE_LINE:READLINE_POINT}}
 		READLINE_POINT=$(( READLINE_POINT + ${#1} ))
@@ -38,12 +56,13 @@ function _fzf_command_bookmark_show() {
 		--preview 'echo -e {-1}; echo; echo -E {..-2} | highlight -S bash -O ansi' \
 		--preview-window=wrap --tac | sed 's/\(.*\)##.*/\1/')
 
-	if [ -z "$ZSH_VERSION" ]; then
-		__ehc "$result"
-	else
+	case "$current_shell" in
+	    "bash")
+		__ehc "$result";;
+	    "zsh")
 		LBUFFER="$LBUFFER$result"
-		zle reset-prompt
-	fi
+		zle reset-prompt;;
+	esac
 }
 
 function _fzf_command_bookmark_add() {
@@ -52,37 +71,40 @@ function _fzf_command_bookmark_add() {
 	local REPLY
 
 	if [ -z "$CMD" ]; then
-		if [ -z "$ZSH_VERSION" ]; then
-			echo -en "\nCommand to add: "
-			read -er CMD
-		else
-			autoload -Uz read-from-minibuffer
-			read-from-minibuffer "Command to add: " $LBUFFER $RBUFFER
-			CMD="$REPLY"
-		fi
+	    case "$current_shell" in
+		"bash")
+		    autoload -Uz read-from-minibuffer
+		    read-from-minibuffer "Command to add: " $LBUFFER $RBUFFER
+		    CMD="$REPLY";;
+		"zsh")
+		    echo -en "\nCommand to add: "
+		    read -er CMD;;
+	    esac
 	fi
 
 	if [ -z "$TITLE" ]; then
-		if [ -z "$ZSH_VERSION" ]; then
-			echo -n "Command title: "
-			read -er TITLE
-		else
-			zle -I
-			read -r "TITLE?Command title: " < /dev/tty
-		fi
+	    case "$current_shell" in
+		"zsh")
+		    echo -n "Command title: "
+		    read -er TITLE;;
+		"bash")
+		    zle -I
+		    read -r "TITLE?Command title: " < /dev/tty;;
+	    esac
 	fi
 
 	echo -E "${CMD}##${TITLE}" >> $FZF_COMMAND_BOOKMARKS_FILE
 }
 
-
-if hash bind 2> /dev/null; then
+check_dependencies
+get_shell
+case "$current_shell" in
+    "bash")
 	bind -x "\"$FZF_COMMAND_BOOKMARKS_ADD\":_fzf_command_bookmark_add"
-	bind -x "\"$FZF_COMMAND_BOOKMARKS_SHOW\":_fzf_command_bookmark_show"
-else
+	bind -x "\"$FZF_COMMAND_BOOKMARKS_SHOW\":_fzf_command_bookmark_show";;
+    "zsh")
 	zle -N fzf-command-bookmark-add-widget _fzf_command_bookmark_add
 	zle -N fzf-command-bookmark-show-widget _fzf_command_bookmark_show
-
 	bindkey "$FZF_COMMAND_BOOKMARKS_ADD" fzf-command-bookmark-add-widget
-	bindkey "$FZF_COMMAND_BOOKMARKS_SHOW" fzf-command-bookmark-show-widget
-fi
+	bindkey "$FZF_COMMAND_BOOKMARKS_SHOW" fzf-command-bookmark-show-widget;;
+esac
